de2a79f1d058c36ef95c19b27066ec2181591057
Ivan solano <ivanheerr13@gmail.com>
2025-02-18 20:04:15 -0500

    mejora el template

--- src/components/PreviewNFT.js
+++ src/components/PreviewNFT.js
@@ -2,31 +2,45 @@ export const PreviewNFT = ($template) => {
     const $clone = document.importNode($template, true); // Se clona el template recibido.
 
     // Se trabaja sobre la instancia creada (clon), accediendo a los elementos dentro de la instancia para modificar su contenido.
-    $clone.querySelector('.header__img').setAttribute('src', './src/images/image-equilibrium.jpg');
-    $clone.querySelector('.header__img').setAttribute('alt', 'Imagen de portada');
+    $clone.querySelector('article > header > img').setAttribute('src', './src/images/image-equilibrium.jpg');
+    $clone.querySelector('article > header > img').setAttribute('alt', 'Imagen de portada');
+    $clone.querySelector('article > header > img').classList.add('header__img'); // Agrega clase css.
 
-    $clone.querySelector('.card__title').textContent = 'Equilibrium #3429';
+    $clone.querySelector('article > section').classList.add('sct-1'); // Agrega clase css.
     
-    $clone.querySelector('.card__detalle').textContent = 'Our Equilibrium collection promotes balance and calm.';
-
-    $clone.querySelector('.icon-ethereum').setAttribute('src', './src/images/icon-ethereum.svg');
-    $clone.querySelector('.icon-ethereum').setAttribute('alt', 'icon-ethereum');
+    $clone.querySelector('article > section > h3').textContent = 'Equilibrium #3429';
+    $clone.querySelector('article > section > h3').classList.add('card__title'); // Agrega clase css.
     
-    $clone.querySelector('.card__precio').textContent = '0.041 ETH';
-
-    $clone.querySelector('.icon-clock').setAttribute('src', './src/images/icon-clock.svg');
-    $clone.querySelector('.icon-clock').setAttribute('alt', 'icon-clock');
-
-    $clone.querySelector('.card__fecha').setAttribute('datetime', '17-02-2025');
-    $clone.querySelector('.card__fecha').textContent = '3 days left';
+    $clone.querySelector('article > section > p').textContent = 'Our Equilibrium collection promotes balance and calm.';
+    $clone.querySelector('article > section > p').classList.add('card__detalle'); // Agrega clase css.
     
-    $clone.querySelector('.avatar').setAttribute('src', './src/images/image-avatar.png');
-    $clone.querySelector('.avatar').setAttribute('alt', 'image-avatar');
+    $clone.querySelector('article > section + section').classList.add('sct-2'); // Agrega clase css.
+
+    $clone.querySelector('article > section + section > figure > img').setAttribute('src', './src/images/icon-ethereum.svg');
+    $clone.querySelector('article > section + section > figure > img').setAttribute('alt', 'icon-ethereum');
+    $clone.querySelector('article > section + section > figure > img').classList.add('icon-ethereum'); // Agrega clase css.
+    
+    $clone.querySelector('article > section + section > figure > img + figcaption').textContent = '0.041 ETH';
+    $clone.querySelector('article > section + section > figure > img + figcaption').classList.add('card__precio'); // Agrega clase css.
+
+    $clone.querySelector('article > section + section > figure + figure > img').setAttribute('src', './src/images/icon-clock.svg');
+    $clone.querySelector('article > section + section > figure + figure > img').setAttribute('alt', 'icon-clock');
+    $clone.querySelector('article > section + section > figure + figure > img').classList.add('icon-clock'); // Agrega clase css.
+
+    $clone.querySelector('article > section + section > figure + figure > img + time').setAttribute('datetime', '2025-02-17');
+    $clone.querySelector('article > section + section > figure + figure > img + time').textContent = '3 days left';
+    $clone.querySelector('article > section + section > figure + figure > img + time').classList.add('card__fecha'); // Agrega clase css.
+    
+    $clone.querySelector('article > footer > img').setAttribute('src', './src/images/image-avatar.png');
+    $clone.querySelector('article > footer > img').setAttribute('alt', 'image-avatar');
+    $clone.querySelector('article > footer > img').classList.add('avatar'); // Agrega clase css.
     
     // Aqui no se usa "textContent" porque plancharia o remplazaria el strong (.nombre) que esta dentro.
-    $clone.querySelector('.autor').prepend(document.createTextNode('Creation of')); // Asi que, se agrega un nuevo nodo de texto como primer hijo. 
+    $clone.querySelector('article > footer > img + p').prepend(document.createTextNode('Creation of')); // Asi que, se agrega un nuevo nodo de texto como primer hijo. 
+    $clone.querySelector('article > footer > img + p').classList.add('autor'); // Agrega clase css.
     
-    $clone.querySelector('.nombre').textContent = 'Jules Wyvern';
+    $clone.querySelector('article > footer > img + p > strong').textContent = 'Jules Wyvern';
+    $clone.querySelector('article > footer > img + p > strong').classList.add('nombre'); // Agrega clase css.
     
     // Se retorna el template clonado y modificado.
     return $clone // Ahora el componente final es una instancia de ese template.
